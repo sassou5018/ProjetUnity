2c970a3c631a1c13d3e5d4bb31e12a9958245a19
Yacinn <sassou.est100@gmail.com>
2022-11-23 00:03:20 +0100

    added slide and slide animation

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -10,10 +10,13 @@ public class Move : MonoBehaviour
     public GameObject player;
     public float speed = 5f;
     public Animator animator;
+    public BoxCollider2D regularCollider;
+    public BoxCollider2D SlideCollider;
 
     private void MovePlayer()
     {
         var input = Input.GetAxisRaw("Horizontal");
+        var slideSpeed = speed*5;
         rb.velocity = new Vector2(input * speed, rb.velocity.y);
         if(input != 0)
         {
@@ -25,11 +28,31 @@ public class Move : MonoBehaviour
         if(input > 0)
         {
             rb.transform.localScale = new Vector2((float)4.0504, (float)4.0504);
+            if (Input.GetKeyDown(KeyCode.LeftControl)){
+                slide(speed, slideSpeed);
+            }
         }
         if (input < 0)
         {
             rb.transform.localScale = new Vector2((float)-4.0504, (float)4.0504);
+            if (Input.GetKeyDown(KeyCode.LeftControl)){
+                slide(speed, slideSpeed);
+            }
         }
+    }
+
+    private void slide(float input, float slideSpeed){
+        regularCollider.enabled = false;
+        SlideCollider.enabled = true;
+        animator.SetTrigger("isSlide");
+        rb.velocity = new Vector2(input * slideSpeed, rb.velocity.y);
+        StartCoroutine(StopSlide());
+    }
+
+    private IEnumerator StopSlide(){
+        yield return new WaitForSeconds(1.0f);
+        regularCollider.enabled = true;
+        SlideCollider.enabled = false;
     }
 
     
